f48f5428a7dbb4485291896b7de10a8e425e38aa
devriazul <riazul56@gmail.com>
2021-10-05 23:15:49 +0600

    Does not work without js library

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -62,6 +62,7 @@
 
 
 <script src="{{'assets/frontend/js/bootstrap.bundle.min.js'}}" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 
 
 </body>
